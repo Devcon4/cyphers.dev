de9f0a9f821bc99c604b5932ca7e0aeb09863eaa
Devcon4 <devcon42@gmail.com>
2019-06-03 22:27:00 -0600

    it almost works!

--- client/main.ts
+++ client/main.ts
@@ -1,19 +1,49 @@
 import { LitElement, html, customElement} from 'lit-element';
-import { Router } from '../router/router';
+import { Router, RouterNode } from '../router/router';
 
 @customElement('dev-app')
 class AppComponent extends LitElement {
+  route(path: string) {
+    return () => Ioc.Router.nav(path);
+  }
+
   render() {
     return html`
       <div>Hello world!</div>
+      <button @click="${this.route('/main')}">main</button>
+      <button @click="${this.route('/about')}">about</button>
+      <button @click="${this.route('/other')}">other</button>
+      <dev-router-outlet></dev-router-outlet>
+      <div>Tailing!</div>
     `;
+  }
+}
+
+@customElement('dev-main')
+class MainComponent extends LitElement {
+  render() {
+    return html`<div>Main!</div>`;
+  }
+}
+
+@customElement('dev-about')
+class AboutComponent extends LitElement {
+  render() {
+    return html`<div>About!<dev-main></dev-main></div>`;
+  }
+}
+
+@customElement('dev-other')
+class OtherComponent extends LitElement {
+  render() {
+    return html`<div>Other!</div>`
   }
 }
 
 @customElement('dev-router-outlet')
 class RouterOutletComponent extends LitElement {
     render() {
-        return html`<div class="dev-router-outlet"></div>`;
+        return html`<div class="dev-router-outlet">`;
     }
 }
 
@@ -22,6 +52,15 @@ export class Ioc {
         beforeRoute(args) {
             console.log(args);
         },
-        routerOutlet: 'dev-router-outlet'
+        routerOutlet: 'dev-router-outlet',
+        tree: new RouterNode({
+          component: AppComponent,
+          path: '',
+          children: [
+            new RouterNode({path: 'main', component: MainComponent }),
+            new RouterNode({path: 'about', component: AboutComponent}),
+            new RouterNode({path: 'other', component: OtherComponent})
+          ]
+        })
     });
 }
--- router/router.ts
+++ router/router.ts
@@ -1,6 +1,6 @@
 import { Guid } from 'guid-typescript';
-import { BehaviorSubject } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { BehaviorSubject, range } from 'rxjs';
+import { tap, filter } from 'rxjs/operators';
 
 export type UrlParamMap<T> = {[k in keyof T]: T[k]}
 
@@ -9,12 +9,17 @@ export class RouterNode {
     path: string;
     children: RouterNode[] = [];
     urlParms: UrlParamMap<unknown> = {};
-    component: componentLike;
+    component: componentLike<any>;
+
+    constructor(args: Partial<RouterNode>) {
+        Object.assign(this, args);
+    }
 }
 
-export type componentLike = {new()} & HTMLElement;
+export type componentLike<T extends {new(): T} & HTMLElement> = T;
 
 export type RouterOptions = {
+    tree: RouterNode,
     beforeRoute: (args: {activeRoute: RouterNode, previousActiveRoute: RouterNode, queryParams: UrlParamMap<unknown>}) => void,
     routerOutlet: string
 };
@@ -26,6 +31,10 @@ export interface devOnDestroy {
     OnDestroy(): Promise<void>;
 }
 
+export interface devOnChanges {
+    OnChanges<T>(data: T): Promise<void>;
+}
+
 export class Router {
 
     public tree: BehaviorSubject<RouterNode> = new BehaviorSubject(undefined);
@@ -33,58 +42,88 @@ export class Router {
 
     public queryParams: BehaviorSubject<UrlParamMap<unknown>> = new BehaviorSubject(undefined);
 
-    private _componentMap = new Map<number, {fragment: DocumentFragment, component: componentLike, guid: Guid}>();
+    private _componentMap = new Map<number, {fragment: DocumentFragment, guid: Guid}>();
     private _options: RouterOptions;
+
+    private _rootFragment: Element;
 
     constructor(options: RouterOptions = {} as RouterOptions) {
         this._options = options;
+
+        this.tree.next(this._options.tree);
         window.addEventListener('popstate', this.onPopState);
 
-        this.activeRoute.pipe(tap(async v => {
-            let depthIndex = 0;
-            let hasRouteChanged = false;
-            let newMap = new Map<Number, {fragment: DocumentFragment, guid: Guid}>();
-            let recurse = (node: RouterNode) => {
-                if(node.children && node.children.length > 0) {
-                    if(this._componentMap.get(depthIndex).guid !== node.id) {
-                        // route changed!
-                        hasRouteChanged = true;
-                    } else {
-                        newMap.set(depthIndex, this._componentMap.get(depthIndex));
-                    }
+        this.activeRoute.pipe(filter(v => !!v), tap(this.onActiveRouteChange.bind(this))).subscribe();
+        setTimeout(() => {
+            this.updateRoute(window.location.pathname);
+        });
+    }
 
-                    for(let i = 0; i < node.children.length; i++) {
-                        let currNode = node.children[i];
-                        let elm = currNode.component.querySelector(this._options.routerOutlet);
-                        if(elm) {
-                            // setup frag here (I think).
-                        }
+    async onActiveRouteChange(v: RouterNode) {
+        let depthIndex = 0;
+        let newMap = new Map<number, {fragment: DocumentFragment, guid: Guid}>();
+        this._componentMap.clear();
+        if(this._rootFragment) {
+            this._rootFragment.removeChild(this._rootFragment.firstElementChild)
+        }
 
-                        recurse(currNode);
-                    }
+        let recurse = async (node: RouterNode) => {
+            if(node.children && node.children.length > 0) {
+                let frag = new DocumentFragment();
+                frag.appendChild(new node.component());
 
-                    depthIndex += 1;
+                newMap.set(depthIndex, {fragment: frag, guid: node.id});
+
+                for(let i = 0; i < node.children.length; i++) {
+                    let currNode = node.children[i];
+                    // console.log(currNode.component);
+                    // let elm = currNode.component.querySelector(this._options.routerOutlet);
+                    // if(elm) {
+                    //     let comp = this._componentMap.get(depthIndex);
+                    //     if (!comp) {
+                    //         let frag = new DocumentFragment();
+                    //         let compElm = frag.appendChild(new currNode.component());
+                    //         comp = {
+                    //             component: compElm,
+                    //             fragment: frag,
+                    //             guid: currNode.id
+                    //         };
+                    //         this._componentMap.set(depthIndex, comp);
+                    //     }
+                    //     elm.appendChild(comp.fragment);
+
+                    //     if(isDevOnChanges(comp.component)) {
+                    //         await comp.component.OnChanges({}/**figure this out later! */);
+                    //     }
+                        // setup frag here (I think).
+                    // }
+
+                    await recurse(currNode);
                 }
+
+                depthIndex += 1;
             }
+        }
 
-            recurse(v);
+        recurse(v);
 
-            if(hasRouteChanged) {
-                let newMapArr = Array.from(newMap);
-                let oldFrags = Array.from(this._componentMap).filter(([i, c]) => newMapArr.some(([vi, vc]) => vc.guid === c.guid)).sort((a, b) => (a[0] > b[0] ? 1 : -1));
+        // let newMapArr = Array.from(newMap);
+        // let oldFrags = Array.from(this._componentMap).filter(([i, c]) => newMapArr.some(([vi, vc]) => vc.guid === c.guid)).sort((a, b) => (a[0] > b[0] ? 1 : -1));
 
-                if(oldFrags && oldFrags.length > 0) {
-                    for(let [i, f] of oldFrags) {
-                        if(isDevOnDestroy(f.component)) {
-                            await f.component.OnDestroy();
-                        }
+        // if(oldFrags && oldFrags.length > 0) {
+        //     for(let [i, f] of oldFrags) {
+        //         console.log(f.fragment);
+        //         f.fragment.removeChild(f.fragment.firstElementChild);
+        //     }
+        // }
 
-                        f.fragment.parentNode.removeChild(f.fragment);
-                    }
-                }
-                
-            }
-        })).subscribe();
+        this._componentMap = newMap;
+
+        let rootComp = this._componentMap.get(0);
+
+        this._rootFragment = document.body.firstElementChild.shadowRoot.querySelector(this._options.routerOutlet).shadowRoot.firstElementChild;
+        this._rootFragment.appendChild(rootComp.fragment);
+
     }
 
     cleanup() {
@@ -92,11 +131,16 @@ export class Router {
     }
 
     onPopState() {
+        console.log('pop state!');
         this.updateRoute(window.location.pathname);
     }
 
     updateRoute(url: string) {
         let results = this.parseUrl(url);
+        console.log('update Route!');
+        console.log(url);
+        console.log(results);
+        if(!results) return;
         
         if(this._options.beforeRoute) this._options.beforeRoute({
             activeRoute: results.activeRoute,
@@ -110,6 +154,7 @@ export class Router {
     }
     
     nav(path: string) {
+        console.log(`nav! ${path}`);
         window.history.pushState(null,null, path);
         this.updateRoute(path);
     }
@@ -129,24 +174,39 @@ export class Router {
     }
 
     searchPathInTree(path: string, depthIndex: number) {
-        let recurse = (index: number, node: RouterNode) => {
+        let recurse = function(index: number, node: RouterNode): {node: RouterNode, index: number} {
+            console.log('searchPathInTree! i: ' + index);
+            console.log(path);
+            console.log(node);
+            if (index === depthIndex) {
+                console.log('correct depth!');
+                if(node.path === path) {
+                    console.log('Path match!');
+                    console.log(node);
+
+                    return {node, index};
+                }
+            };
+
+            let newIndex = index + 1;
             if(node.children && node.children.length > 0) {
-                if (index = depthIndex) {
-                    if(node.path === path) {
-                        return {node, index};
-                    }
-                };
                 for(let n of node.children) {
-                    if(index < depthIndex && n.children && n.children.length > 0) recurse(index++, n);
+                    if(index < depthIndex) {
+                        let res = recurse(newIndex, n);
+                        if(res) return res;
+                    }
                 }
             }
         }
-
-        return recurse(0, this.tree.getValue());
+        let res = recurse(0, this.tree.getValue());
+        return res;
     }
 
     parseUrl(url: string) {
+        console.group('parseUrl!');
         let parts = url.split('/');
+        console.log(parts);
+        console.log()
         let urlParams = new Set<string>();
         let queryParams = new Map<string, string>();
 
@@ -155,6 +215,7 @@ export class Router {
         let treeIndex = 0;
 
         for(let i = 0; i < parts.length; i++) {
+            console.log('part: ' + parts[i]);
             let isUrlParam = (/(:)\w+/).test(parts[i]); // This is wrong, urlParams will be like user/4/details not user/:4/details
             let isQueryParam = (/(\?)\w+=".*"/g).test(parts[i])
             if(isQueryParam) {
@@ -162,13 +223,13 @@ export class Router {
             }
 
             let matchNode = this.searchPathInTree(parts[i], treeIndex);
-
             if(matchNode) {
+                console.log(matchNode);
                 treeIndex++;
                 if(!activeRoute) {
-                    activeRoute = matchNode.node;
+                    activeRoute = { ...matchNode.node, children: [] };
                 } else {
-                    let holder = matchNode.node;
+                    let holder = {...matchNode.node};
                     holder.children.push(activeRoute);
                     activeRoute = holder;
                 }
@@ -180,7 +241,28 @@ export class Router {
 
         }
 
-        return { urlParamMap: urlParams, queryParamMap: queryParams, activeRoute };
+        console.log(activeRoute);
+        let newRoot: RouterNode;
+
+        let invertTree = (node: RouterNode) => {
+            if(node.children && node.children.length > 0) {
+                for(let n of node.children) {
+                    invertTree(n);
+                }
+            }
+
+            if(!newRoot) {
+                newRoot = node;
+            } else {
+                let holder = {...node};
+                holder.children.push(newRoot);
+                newRoot = holder;
+            }
+        }
+
+        invertTree(activeRoute);
+        console.groupEnd();
+        return { urlParamMap: urlParams, queryParamMap: queryParams, activeRoute: newRoot };
     }
 }
 
@@ -190,4 +272,8 @@ export function isDevOnDestroy(comp: any): comp is devOnDestroy {
 
 export function isDevOnInit(comp: any): comp is devOnInit {
     return !!comp.OnInit;
+}
+
+export function isDevOnChanges(comp: any): comp is devOnChanges {
+    return !!comp.OnChanges;
 }
